9a9d6b4732af9993e8adbd3bb75dc9dc2c32bb22
Clemens <c.lottermoser@campus.tu-berlin.de>
2022-09-22 15:36:39 +0200

    added firmware update page

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, send_from_directory, request, make_response
+import os
+
+from flask import Flask, render_template, send_from_directory, request, make_response, flash, redirect, url_for
 import datetime
 
+from werkzeug.utils import secure_filename
+
 app = Flask(__name__)
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1000 * 1000
 
 
 @app.route('/')
@@ -25,11 +30,44 @@ def info():
     return r
 
 
+@app.route('/up')
+def up():
+    r = make_response(render_template('update.html'))
+    r.headers['Server'] = 'Tasmota/12.1.1 (ESP8266EX)'
+    return r
+
+
+def allowed_file(filename):
+    return True
+
+
+@app.route('/u2', methods=['POST'])
+def u2():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'u2' not in request.files:
+            return render_template('update_no_file_selected.html')
+        file = request.files['u2']
+        # If the user does not select a file, the browser submits an
+        # empty file without a filename.
+        if file.filename == '':
+            return render_template('update_no_file_selected.html')
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join('uploaded_files/', filename))
+            return render_template('upload_success.html')
+
+
+@app.errorhandler(413)
+def too_large(e):
+    return render_template('update_too_large.html')
+
+
 def uptime():
     start_date = datetime.datetime(2022, 9, 19, 14, 37, 3)
     duration = datetime.datetime.now() - start_date
     hours = int(duration.total_seconds() / 60 / 60)
-    minutes = int(duration.total_seconds() / 60) - hours*60
+    minutes = int(duration.total_seconds() / 60) - hours * 60
     seconds = int(duration.total_seconds() % 60)
     string = f'{duration.days}T{hours}:{minutes}:{seconds}'  # TODO leading zero
 
